79ba5c3b7e723dedeeb1581487550a51704a7709
ballll16 <ahmadhamidbalya@gmail.com>
2025-01-04 16:41:37 +0700

    ✨ feat(user): get current user api
    make get current user data api includes its service, controller, and test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { WebResponse } from '@model/web.model';
 import {
@@ -6,6 +6,8 @@ import {
   RegisterUserRequest,
   UserResponse,
 } from '@model/user.model';
+import { Auth } from '@common/auth.decorator';
+import { User } from '@prisma/client';
 
 @Controller('/api/users')
 export class UserController {
@@ -27,6 +29,14 @@ export class UserController {
     @Body() req: LoginUserRequest,
   ): Promise<WebResponse<UserResponse>> {
     const result = await this.userService.login(req);
+    return {
+      data: result,
+    };
+  }
+
+  @Get('/current')
+  async get(@Auth() user: User): Promise<WebResponse<UserResponse>> {
+    const result = await this.userService.get(user);
     return {
       data: result,
     };
